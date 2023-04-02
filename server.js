cf576f631770972c452bd47e4734489fb7f143c4
Dandi Lesmana <dandilesmana.if21@gmail.com>
2023-04-01 22:56:13 -0700

    ad response header and response body

--- server.js
+++ server.js
@@ -10,23 +10,37 @@ const http = require('http');
 
 //Request listener memiliki 2 parameter, yakni request dan response
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
 
     const { method, url } = request;
 
     // Routing Request
     if (url === '/') {
         if (method === 'GET') {
-            response.end('<h1>Ini adalah homepage</h1>');
+
+            response.statusCode = 200;
+            //change JavaScript object to JSON string
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
         } else {
-            // response bila client tidak menggunakan GET
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
         }
 
     } else if (url === '/about') {
         if (method === 'GET') {
-            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+
+            response.statusCode = 200;
+
+            //change JavaScript object to JSON string
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }));
         } else if (method === 'POST') {
             // array null berfungsi untuk menampung buffer pada stream
             let body = [];
@@ -42,14 +56,25 @@ const requestListener = (request, response) => {
 
                 // untuk mengubah JSON string menjadi JavaScript objek
                 const { name } = JSON.parse(body);
-                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }));
             });
         } else {
-            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
-            // respons bila client tidak menggunakan GET ataupun POST
+
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses menggunakan ${method} request`,
+            }));
         }
     } else {
-        response.end('<h1>Halaman tidak ditemukan!</h1>');
+
+        response.statusCode = 404;
+        //ubah objek JavaScript menjadi JSON string
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }));
     }
 
 };
